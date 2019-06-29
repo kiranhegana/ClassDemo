229e0f590e1d41df1b418b26e08c6766204e7e27
Kiran <51105060+kiranhegana@users.noreply.github.com>
2019-06-29 10:40:48 +0530

    Add files via upload

---
+++ ClassDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace ClassDemo
+{
+    class Student
+    {
+        string _firstName;
+        string _lastName;
+
+        public Student(string Fname, string Lname)
+        {
+            _firstName = Fname;
+            _lastName = Lname;
+        }
+        public void FullName()
+        {
+            Console.WriteLine("Full Name of Student = {0} {1}",_firstName,_lastName);
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            string FN, LN;
+            Console.WriteLine("Enter the First Name  ");
+            FN = Console.ReadLine();
+
+            Console.WriteLine("Enter the Last Name  ");
+            LN = Console.ReadLine();
+
+            Student std = new Student(FN,LN);
+            std.FullName();
+
+            Console.ReadLine();
+        }
+    }
+}
